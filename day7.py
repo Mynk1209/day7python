54b8b490cc81d0583b7da7cb59fc56e8fb4fa57d
Mayank Tyagi <mayank.morta1209@gmail.com>
2024-07-31 14:39:57 +0530

    summer internship

---
+++ day7.py
@@ -0,0 +1,56 @@
+#functions
+# def add_two(num1 ,num2):
+#     # return num1,num2
+#     output = num1 + num2
+#     return output
+
+# a = int(input('enter first number : '))
+# b = int(input('enter second number : '))
+
+# #calling
+# # output = add_two(num1=a,num2=b)
+# output = add_two(a,b)
+# print("your addition is : ", output)
+
+# ls= [42,34,23,45,57,75,45,56,44,49,43,78]
+# print(len(ls))
+
+# def my_len(ls) :
+#     count=0
+#     for item in ls:
+#         count +=1
+#     return  count    
+# ls= [42,34,23,45,57,75,45,56,44,49,43,78]
+# print(my_len(ls))
+
+# total = 0
+# for num in ls:
+#     total += num
+# print(total)
+
+
+# average_finder()
+# even_finder() 
+# average_finder_even()
+# my_min()
+# my_max()
+manager_name = "Dharmpal ji"
+manager_address = "Bihar"
+
+eployee_list = ['rohit','rahul','mohit','suresh','jagdessh'] 
+
+rohit_id = 'hello@gmail.com'
+rohit_pass = "124@786"
+
+def rohit_hii():
+    print("Employee name : ","Rohit")
+    print('Rohit id : ',rohit_id)
+    print('ROhit password : ',rohit_pass)
+
+def manager_hii():
+    print("Manager name : ","Dharmpal")
+    print('Dharmpal id : ',manager_id)
+    print('Dharmpal password : ',manager_pass)
+
+manager_id = 'manager_boss@gmail.com'
+manager_pass = 'boss123456'
